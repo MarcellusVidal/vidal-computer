7016e3b6eb068b86f14c1dd07d41d12b1a47749a
Marcellus <arquiteto.marcellus@gmail.com>
2022-08-31 17:21:50 -0300

    Add product (image, description, price) and route

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,15 +4,19 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
+import { FooterComponent } from './footer/footer.component';
+import { AppOutinModule } from './app-outin.module';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HeaderComponent
+    HeaderComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    AppOutinModule
   ],
   providers: [],
   bootstrap: [AppComponent]
